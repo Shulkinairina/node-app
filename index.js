d331ff7c97f99b473a483edf475ac8beceda36dc
Irina <shulkinairina@yandex.ru>
2024-12-19 22:20:11 +0300

    Домашняя работа 9

---
+++ index.js
@@ -0,0 +1,18 @@
+let userName = 'Legion';
+console.log(userName);
+
+function showGreeting(name) {
+  return name;
+}
+
+console.log(showGreeting('Macbook'));
+
+
+
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+Sentry.init({
+  dsn: 'YOUR_SENTRY_DSN',
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
